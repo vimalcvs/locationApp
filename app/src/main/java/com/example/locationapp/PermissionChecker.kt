b6018b0d4425b8fde9b340b88d720f1cb488233b
rawani53 <work.pranav53@gmail.com>
2024-11-24 11:56:00 +0530

    fusedlocationproviderclient used, location callback called, viewmaodel internal variable update linked.

--- app/src/main/java/com/example/locationapp/PermissionChecker.kt
+++ app/src/main/java/com/example/locationapp/PermissionChecker.kt
@@ -4,8 +4,27 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 class PermissionChecker (val context : Context){
+
+    private val _fusedlocationclient : FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+    fun  toallocateLresultfromFLPCtoCallbackandthenviewmodel(viewModel: LocationViewModel){
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            locationResult.lastLocation?.let {
+                val location = LocationData(Lattitude = it.latitude , Longitude = it.longitude)
+                viewModel.update_locationValue(location)
+            }
+        }
+
+    }}
 
     fun Questionlocationpermission(context : Context) : Boolean {
 
